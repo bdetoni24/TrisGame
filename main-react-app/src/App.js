35ce80b6e509d5286934bf6c8389c31ec73a2471
bernardodetoni <bernardodetoni@pasubiotecnologia.it>
2023-07-05 11:56:38 +0200

    Update2

--- main-react-app/src/App.js
+++ main-react-app/src/App.js
@@ -8,19 +8,19 @@ function App() {
       <h1>Tris Game</h1>
       <table>
         <tr>
-          <td >Cella 1</td>
-          <td>Cella 2</td>
-          <td>Cella 3</td>
+          <td >1</td>
+          <td>2</td>
+          <td>3</td>
         </tr>
         <tr>
-          <td>Cella 4</td>
-          <td>Cella 5</td>
-          <td>Cella 6</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
         </tr>
         <tr>
-          <td>Cella 7</td>
-          <td>Cella 8</td>
-          <td>Cella 9</td>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
         </tr>
       </table>
     </div>

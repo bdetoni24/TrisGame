53a5589ec8b2d2b030302219fb60b457209b7bc0
bernardodetoni <bernardodetoni@pasubiotecnologia.it>
2023-07-05 13:45:12 +0200

    Update3

--- main-react-app/src/App.js
+++ main-react-app/src/App.js
@@ -1,30 +1,46 @@
 import './App.css';
 
-function App() {
-  
+export default function App() {
+  var arrCelle = 1;
 
   return (
     <div className="App">
       <h1>Tris Game</h1>
-      <table>
-        <tr>
-          <td >1</td>
-          <td>2</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
-        </tr>
-        <tr>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-        </tr>
-      </table>
+      <Table/>
     </div>
   );
 }
 
-export default App;
+
+function clickCell(nCella){
+  console.log("È stata cliccata la cella "+nCella+" "+arrCelle);
+  
+}
+
+function Table(){
+  return (
+    <table>
+    <thead>
+    </thead>
+    <tbody>
+      <tr>
+        <td id='1' onClick={() => clickCell("1")}>Cella 1</td>
+        <td onClick={() => clickCell("2")}>Cella 2</td>
+        <td onClick={() => clickCell("3")}>Cella 3</td>
+      </tr>
+      <tr>
+        <td onClick={() => clickCell("4")}>Cella 4</td>
+        <td onClick={() => clickCell("5")}>Cella 5</td>
+        <td onClick={() => clickCell("6")}>Cella 6</td>
+      </tr>
+      <tr>
+        <td onClick={() => clickCell("7")}>Cella 7</td>
+        <td onClick={() => clickCell("8")}>Cella 8</td>
+        <td onClick={() => clickCell("9")}>Cella 9</td>
+      </tr>
+    </tbody>
+    <tfoot>
+    </tfoot>
+  </table>
+  );
+}

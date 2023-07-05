98ec98ae121735c223c2224234b2bc7b7d308522
bernardodetoni <bernardodetoni@pasubiotecnologia.it>
2023-07-05 16:03:59 +0200

    Update4-Rifatto l'intero progetto da fondo

--- main-react-app/src/App.js
+++ main-react-app/src/App.js
@@ -1,46 +1,75 @@
 import './App.css';
 
-export default function App() {
-  var arrCelle = 1;
+function Table(nRow,nColumn){
+  const team=false;
+  const clickedCells = [
+    {id:1, clicked:false, team: "n"},
+    {id:2, clicked:false, team: "n"},
+    {id:3, clicked:false, team: "n"},
+    {id:4, clicked:false, team: "n"},
+    {id:5, clicked:false, team: "n"},
+    {id:6, clicked:false, team: "n"},
+    {id:7, clicked:false, team: "n"},
+    {id:8, clicked:false, team: "n"},
+    {id:9, clicked:false, team: "n"},
+  ];
 
-  return (
-    <div className="App">
-      <h1>Tris Game</h1>
-      <Table/>
+
+  function tableClicked(nCella){
+    if(!clickedCells[nCella-1].clicked){
+      let char;
+      if(team){
+        char="o";
+      }
+      else{
+        char="x";
+      }
+
+      document.getElementById(nCella+"").innerHTML= "<td nameClass=\""+char+"\">"+char+"</td>"
+
+      clickedCells[nCella-1].clicked=true;
+      team =! team;
+      console.log("È stata cliccata la casella "+nCella);
+    }
+  }
+
+  return(
+    <div>
+      <table>
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td id="1" onClick={()=>tableClicked(1)}>1</td>
+            <td id="2" onClick={()=>tableClicked(2)}>2</td>
+            <td id="3" onClick={()=>tableClicked(3)}>3</td>
+          </tr>
+          <tr>
+            <td id="4" onClick={()=>tableClicked(4)}>4</td>
+            <td id="5" onClick={()=>tableClicked(5)}>5</td>
+            <td id="6" onClick={()=>tableClicked(6)}>6</td>
+          </tr>
+          <tr>
+            <td id="7" onClick={()=>tableClicked(7)}>7</td>
+            <td id="8" onClick={()=>tableClicked(8)}>8</td>
+            <td id="9" onClick={()=>tableClicked(9)}>9</td>
+          </tr>
+        </tbody>
+        <tfoot></tfoot>
+      </table>
     </div>
   );
 }
 
 
-function clickCell(nCella){
-  console.log("È stata cliccata la cella "+nCella+" "+arrCelle);
-  
-}
 
-function Table(){
-  return (
-    <table>
-    <thead>
-    </thead>
-    <tbody>
-      <tr>
-        <td id='1' onClick={() => clickCell("1")}>Cella 1</td>
-        <td onClick={() => clickCell("2")}>Cella 2</td>
-        <td onClick={() => clickCell("3")}>Cella 3</td>
-      </tr>
-      <tr>
-        <td onClick={() => clickCell("4")}>Cella 4</td>
-        <td onClick={() => clickCell("5")}>Cella 5</td>
-        <td onClick={() => clickCell("6")}>Cella 6</td>
-      </tr>
-      <tr>
-        <td onClick={() => clickCell("7")}>Cella 7</td>
-        <td onClick={() => clickCell("8")}>Cella 8</td>
-        <td onClick={() => clickCell("9")}>Cella 9</td>
-      </tr>
-    </tbody>
-    <tfoot>
-    </tfoot>
-  </table>
-  );
-}
+export default function App(){
+
+
+
+  return( 
+    <div>
+      <h1>Tris Game</h1>
+      <Table/>
+    </div>
+    );
+};

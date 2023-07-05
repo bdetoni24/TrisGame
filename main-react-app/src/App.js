232d517c1534441863160cb643cd62ba5c169596
bernardodetoni <bernardodetoni@pasubiotecnologia.it>
2023-07-05 11:50:07 +0200

    Update1

--- main-react-app/src/App.js
+++ main-react-app/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tris Game</h1>
+      <table>
+        <tr>
+          <td >Cella 1</td>
+          <td>Cella 2</td>
+          <td>Cella 3</td>
+        </tr>
+        <tr>
+          <td>Cella 4</td>
+          <td>Cella 5</td>
+          <td>Cella 6</td>
+        </tr>
+        <tr>
+          <td>Cella 7</td>
+          <td>Cella 8</td>
+          <td>Cella 9</td>
+        </tr>
+      </table>
     </div>
   );
 }
